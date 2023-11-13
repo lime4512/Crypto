b4fe7efc86c2af40813a8e0e7982ece10465cdb4
Alex Kruglov <lime4512@mail.ru>
2023-11-14 00:19:09 +0300

    add card style

--- src/components/cards/Card.tsx
+++ src/components/cards/Card.tsx
@@ -1,12 +1,21 @@
 import { useEffect, FunctionComponent, useState } from 'react'
 import axios from 'axios'
+import Image from 'next/image'
+import './Card.css'
 
 interface Props {
 	cryptoValue: string
+	cryptoTitle: string
+	cryptoImg: string
 }
 
-export const Card: FunctionComponent<Props> = ({ cryptoValue }) => {
-	const [cryptoText, setCryptoText] = useState('')
+export const Card: FunctionComponent<Props> = ({
+	cryptoValue,
+	cryptoTitle,
+	cryptoImg,
+}) => {
+	const [cryptoPrice, setCryptoPrice] = useState('')
+	const [cryptoChange, setCryptoChange] = useState('')
 
 	useEffect(() => {
 		async function fetchExchangeRates() {
@@ -14,11 +23,12 @@ export const Card: FunctionComponent<Props> = ({ cryptoValue }) => {
 
 			axios
 				.get(
-					`https://min-api.cryptocompare.com/data/price?fsym=${value}&tsyms=USD&api_key={793950bc36e2be95361fa5ab812e8fa52e2bfcbacd5d558c2c377f70be8d04f7}`
+					`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${value}&tsyms=USD`
 				)
 				.then(response => {
 					console.log(response)
-					setCryptoText(response.data.USD)
+					setCryptoPrice(response.data.DISPLAY[value].USD.PRICE)
+					setCryptoChange(response.data.DISPLAY[value].USD.CHANGEPCT24HOUR)
 				})
 				.catch(error => {
 					// Обработка ошибки
@@ -29,9 +39,27 @@ export const Card: FunctionComponent<Props> = ({ cryptoValue }) => {
 	}, [cryptoValue])
 
 	return (
-		<div>
-			<h1>{cryptoValue}</h1>
-			<span>{cryptoText}</span>
+		<div className='card-item-content'>
+			<div className='card-top'>
+				<div className='card-total-title'>
+					<div className='card-img'>
+						<Image src={cryptoImg} alt='Картинка' height={24} width={24} />
+					</div>
+					<div className='card-text'>
+						<p className='cart-name'>{cryptoTitle}</p>
+						<p className='cart-reduction'>{cryptoValue}</p>
+					</div>
+				</div>
+				{Number(cryptoChange) > 0 ? (
+					<Image src='/arrow-up.png' alt='img' height={18} width={18} />
+				) : (
+					<Image src='/arrow-down.png' alt='img' height={18} width={18} />
+				)}
+			</div>
+			<span className='card-price'>{cryptoPrice}</span>
+			<p className={Number(cryptoChange) > 0 ? 'cart-change1' : 'cart-change2'}>
+				{cryptoChange}
+			</p>
 		</div>
 	)
 }
--- src/components/cards/CardsList.tsx
+++ src/components/cards/CardsList.tsx
@@ -1,15 +1,32 @@
 import { Card } from './Card'
+import './CardList.css'
 
 export const CardsList = () => {
 	return (
-		<div>
-			<Card cryptoValue={'BTC'} />
-			<Card cryptoValue={'ETH'} />
-			<Card cryptoValue={'BNB'} />
-			<Card cryptoValue={'DOGE'} />
-			<Card cryptoValue={'SOL'} />
-			<Card cryptoValue={'TRX'} />
-			<Card cryptoValue={'TON'} />
+		<div className='cards-list-content'>
+			<Card
+				cryptoValue={'BTC'}
+				cryptoTitle={'Биткоин'}
+				cryptoImg={'/btc.png'}
+			/>
+			<Card
+				cryptoValue={'ETH'}
+				cryptoTitle={'Эфириум'}
+				cryptoImg={'/eth.png'}
+			/>
+			<Card cryptoValue={'BNB'} cryptoTitle={'BNB'} cryptoImg={'/bnb.png'} />
+			<Card
+				cryptoValue={'DOGE'}
+				cryptoTitle={'DogeCoin'}
+				cryptoImg={'/doge.png'}
+			/>
+			<Card
+				cryptoValue={'FIL'}
+				cryptoTitle={'Filecoin'}
+				cryptoImg={'/fil.png'}
+			/>
+			<Card cryptoValue={'TRX'} cryptoTitle={'TRON'} cryptoImg={'/trx.png'} />
+			<Card cryptoValue={'OKB'} cryptoTitle={'OKB'} cryptoImg={'/okb.png'} />
 		</div>
 	)
 }
