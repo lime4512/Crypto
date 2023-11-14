12a038ee21d081c273788af4e16f1d5929a2f868
Alex Kruglov <lime4512@mail.ru>
2023-11-14 22:23:46 +0300

    add Marquee auto slider

--- src/components/cards/CardsList.tsx
+++ src/components/cards/CardsList.tsx
@@ -1,90 +1,54 @@
 import { Card } from './Card'
 import './CardList.css'
+import Marquee from 'react-fast-marquee'
 
 export const CardsList = () => {
 	return (
 		<div className='cards-list-content'>
-			<div className='ticker'>
-				<div className='ticker-wrapper'>
-					<div className='ticker-item'>
-						<Card
-							cryptoValue={'BTC'}
-							cryptoTitle={'Биткоин'}
-							cryptoImg={'/btc.png'}
-						/>
-					</div>
-					<div className='ticker-item'>
-						<Card
-							cryptoValue={'ETH'}
-							cryptoTitle={'Эфириум'}
-							cryptoImg={'/eth.png'}
-						/>
-					</div>
-					<div className='ticker-item'>
-						<Card
-							cryptoValue={'BNB'}
-							cryptoTitle={'BNB'}
-							cryptoImg={'/bnb.png'}
-						/>
-					</div>
-					<div className='ticker-item'>
-						<Card
-							cryptoValue={'DOGE'}
-							cryptoTitle={'DogeCoin'}
-							cryptoImg={'/doge.png'}
-						/>
-					</div>
-					<div className='ticker-item'>
-						<Card
-							cryptoValue={'FIL'}
-							cryptoTitle={'Filecoin'}
-							cryptoImg={'/fil.png'}
-						/>
-					</div>
-					<div className='ticker-item'>
-						<Card
-							cryptoValue={'TRX'}
-							cryptoTitle={'TRON'}
-							cryptoImg={'/trx.png'}
-						/>
-					</div>
-					<div className='ticker-item'>
-						<Card
-							cryptoValue={'OKB'}
-							cryptoTitle={'OKB'}
-							cryptoImg={'/okb.png'}
-						/>
-					</div>
-					<div className='ticker-item'>
-						<Card
-							cryptoValue={'BTC'}
-							cryptoTitle={'Биткоин'}
-							cryptoImg={'/btc.png'}
-						/>
-					</div>
-					<div className='ticker-item'>
-						<Card
-							cryptoValue={'ETH'}
-							cryptoTitle={'Эфириум'}
-							cryptoImg={'/eth.png'}
-						/>
-					</div>
-					<div className='ticker-item'>
-						<Card
-							cryptoValue={'FIL'}
-							cryptoTitle={'Filecoin'}
-							cryptoImg={'/fil.png'}
-						/>
-					</div>
-					<div className='ticker-item'>
-						<Card
-							cryptoValue={'BNB'}
-							cryptoTitle={'BNB'}
-							cryptoImg={'/bnb.png'}
-						/>
-					</div>
+			<Marquee>
+				<div className='ticker-item'>
+					<Card
+						cryptoValue={'BTC'}
+						cryptoTitle={'Биткоин'}
+						cryptoImg={'/btc.png'}
+					/>
 				</div>
-			</div>
+				<div className='ticker-item'>
+					<Card
+						cryptoValue={'ETH'}
+						cryptoTitle={'Эфириум'}
+						cryptoImg={'/eth.png'}
+					/>
+				</div>
+				<div className='ticker-item'>
+					<Card
+						cryptoValue={'BNB'}
+						cryptoTitle={'BNB'}
+						cryptoImg={'/bnb.png'}
+					/>
+				</div>
+				<div className='ticker-item'>
+					<Card
+						cryptoValue={'DOGE'}
+						cryptoTitle={'DogeCoin'}
+						cryptoImg={'/doge.png'}
+					/>
+				</div>
+				<div className='ticker-item'>
+					<Card
+						cryptoValue={'FIL'}
+						cryptoTitle={'Filecoin'}
+						cryptoImg={'/fil.png'}
+					/>
+				</div>
+				<div className='ticker-item'>
+					<Card
+						cryptoValue={'TRX'}
+						cryptoTitle={'TRON'}
+						cryptoImg={'/trx.png'}
+					/>
+				</div>
+			</Marquee>
 		</div>
 	)
 }
